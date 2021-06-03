a9d4927f7e600c40b0fe8194ac63f158a84fa13b
Graham Hadgraft <graham.hadgraft@gmail.com>
2021-06-03 22:31:42 +0100

    Add support to render controllers from rackup
    Rather than rendering a string load a controller class and use the
    simple routing action to define text to render.
    
    Adds support for setting a homepage by either using an index.html file
    in the public folder or creating a home controller with an action of
    index

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,13 +1,49 @@
 require "rulers/version"
 require "rulers/array"
+require 'rulers/routing'
 
 module Rulers
   class Error < StandardError; end
   
   class Application
     def call(env)
-      [200, { 'Content-Type' => 'text/html'},
-        ["Hello from Ruby on Rulers!"]]
+      if env['PATH_INFO'] == 'favicon.ico'
+        return [404, { 'Content-Type' => 'text/html'}, []]
+      end
+
+      if env['PATH_INFO'] == '/'
+        if File.exists?("public/index.html")
+          return [200, { 'Content-Type' => 'text/html'},
+            [File.read("public/index.html")]]
+        else
+          return [200, { 'Content-Type' => 'text/html'},
+          [HomeController.new(env).index]]
+        end
+      end
+
+      begin
+        klass, action = get_controller_and_action(env)
+        controller = klass.new(env)
+        text = controller.send(action)
+        [200, { 'Content-Type' => 'text/html'},
+          [text]]
+      rescue => e
+        if env['environment'] == 'production'
+          [500, { 'Content-Type' => 'text/html'}, ["Something went wrong"]]
+        else
+          raise
+        end
+      end
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
